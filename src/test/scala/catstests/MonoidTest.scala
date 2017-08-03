5ff224444665f4bcf5d7678cafbd7141477696d3
Ralf Oenning <ralf@invicto.net>
2017-08-03 21:46:11 +0100

    combining ignoring second Invalid if any

--- src/test/scala/catstests/MonoidTest.scala
+++ src/test/scala/catstests/MonoidTest.scala
@@ -4,6 +4,17 @@ import cats.Monoid
 import org.scalatest.{FlatSpec, Matchers}
 
 
+sealed trait Expr
+
+final case class Add(expr1: Expr, expr2: Expr) extends Expr
+final case class Multiply(expr1: Expr, expr2: Expr) extends Expr
+final case class FormCtx(value: String) extends Expr
+final case class AuthCtx(value: AuthInfo) extends Expr
+final case class EeittCtx(value: Eeitt) extends Expr
+final case class Constant(value: String) extends Expr
+
+
+
 sealed trait ValidationResult {
   type Opt[A] = Either[InvalidState, A]
   def toEither: Opt[Unit] = this match {
@@ -18,8 +29,8 @@ case class Invalid(reason: String) extends ValidationResult
 object ValidationResult {
 
   implicit val validationResultMonoid = new Monoid[ValidationResult] {          //<======= implicit required (1)
-    def empty: ValidationResult = Valid
-    def combine(x: ValidationResult, y: ValidationResult): ValidationResult = (x, y) match {
+  def empty: ValidationResult = Valid
+    def combine(x: ValidationResult, y: ValidationResult): ValidationResult = (x, y) match {    //combining - ignoring second Invalid reason...
       case (Valid, Valid) => Valid
       case (i @ Invalid(_), _) => i
       case (_, i @ Invalid(_)) => i
@@ -27,19 +38,16 @@ object ValidationResult {
   }
 }
 
-sealed trait Expr {
-  def validate(fieldNames: List[String]): ValidationResult = {
 
-    def checkFields(field1: Expr, field2: Expr): ValidationResult = {
-      val checkField1 = field1.validate(fieldNames)
-      val checkField2 = field2.validate(fieldNames)
-      val monoid: Monoid[ValidationResult] = Monoid[ValidationResult]                         //<======= (1) is required here
-      monoid.combineAll(List(checkField1, checkField2))
-    }
+object ExprLogic {
 
-    this match {
-      case Add(field1, field2) => checkFields(field1, field2)
-      case Multiply(field1, field2) => checkFields(field1, field2)
+  val monoid: Monoid[ValidationResult] = Monoid[ValidationResult]                         //<======= (1) is required here
+
+  def validate(expr: Expr, fieldNames: List[String]): ValidationResult = {
+
+    expr match {
+      case Add(expr1, expr2) => monoid.combineAll(List(validate(expr1, fieldNames), validate(expr2, fieldNames)))
+      case Multiply(expr1, expr2) => monoid.combineAll(List(validate(expr1, fieldNames), validate(expr2, fieldNames)))
       case FormCtx(value) =>
         if (fieldNames.contains(value))
           Valid
@@ -51,13 +59,6 @@ sealed trait Expr {
     }
   }
 }
-
-final case class Add(field1: Expr, field2: Expr) extends Expr
-final case class Multiply(field1: Expr, field2: Expr) extends Expr
-final case class FormCtx(value: String) extends Expr
-final case class AuthCtx(value: AuthInfo) extends Expr
-final case class EeittCtx(value: Eeitt) extends Expr
-final case class Constant(value: String) extends Expr
 
 
 sealed trait Operation
@@ -81,10 +82,13 @@ case class InvalidState(errorMsg: String)
 
 class MonoidTest extends FlatSpec with Matchers {
 
-  "" should "" in {
-    println(Add(FormCtx("abc"), FormCtx("def")).validate(List("abc", "def")))
-    println(Add(FormCtx("abd"), FormCtx("def")).validate(List("abc", "def")))
-    println(Add(FormCtx("abd"), FormCtx("ded")).validate(List("abc", "def")))
+  import ExprLogic.validate
+
+  "combining validations" should "work" in {
+    val listOfFieldNames = List("abc", "def")
+    validate(Add(FormCtx("abc"), FormCtx("def")), listOfFieldNames) shouldBe(Valid)
+    validate(Add(FormCtx("abd"), FormCtx("def")), listOfFieldNames) shouldBe(Invalid("Form field 'abd' is not defined in form template."))
+    validate(Add(FormCtx("abd"), FormCtx("ded")), listOfFieldNames) shouldBe(Invalid("Form field 'abd' is not defined in form template."))
   }
 
 
