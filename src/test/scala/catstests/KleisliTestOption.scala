5fa8790d00fc90c430ccbca859de9fa89e3f9d30
Ralf Oenning <ralf@invicto.net>
2017-11-05 13:51:24 +0000

    for-comp vs Kleisli

--- src/test/scala/catstests/KleisliTestOption.scala
+++ src/test/scala/catstests/KleisliTestOption.scala
@@ -15,6 +15,42 @@ import cats.implicits._
   *
   *
   */
+/**
+  *
+  * Sometimes, our functions will need to return monadic values. For instance, consider the following set of functions.
+
+val parse: String => Option[Int] =
+  s => if (s.matches("-?[0-9]+")) Some(s.toInt) else None
+
+val reciprocal: Int => Option[Double] =
+  i => if (i != 0) Some(1.0 / i) else None
+As it stands we cannot use Function1.compose (or Function1.andThen) to compose these two functions. The output type of parse is Option[Int] whereas the input type of reciprocal is Int.
+
+This is where Kleisli comes into play.
+
+  At it’s core, Kleisli[F[_], A, B] is just a wrapper around the function A => F[B]. Depending on the properties of the F[_], we can do different things with Kleislis. For instance, if F[_] has a FlatMap[F] instance (we can call flatMap on F[A] values), we can compose two Kleislis much like we can two functions.
+
+import cats.FlatMap
+import cats.implicits._
+
+final case class Kleisli[F[_], A, B](run: A => F[B]) {
+  def compose[Z](k: Kleisli[F, Z, A])(implicit F: FlatMap[F]): Kleisli[F, Z, B] =
+    Kleisli[F, Z, B](z => k.run(z).flatMap(run))
+}
+Returning to our earlier example:
+
+// Bring in cats.FlatMap[Option] instance
+import cats.implicits._
+
+val parse = Kleisli((s: String) => if (s.matches("-?[0-9]+")) Some(s.toInt) else None)
+
+val reciprocal = Kleisli((i: Int) => if (i != 0) Some(1.0 / i) else None)
+
+val parseAndReciprocal = reciprocal.compose(parse)
+Kleisli#andThen can be defined similarly.
+  *
+  *
+  */
 class KleisliTestOption extends FlatSpec with Matchers {
 
     case class Make(id: Int, name: String)
@@ -83,7 +119,7 @@ class KleisliTestOption extends FlatSpec with Matchers {
     val f0: (Int) => Option[Int] = foo(0)
     val f1 = foo(1)
 
-    val composed = Kleisli(f0).andThen(Kleisli(f1))
+    val composed: Kleisli[Option, Int, Int] = Kleisli(f0).andThen(Kleisli(f1))
 
     println(f0(0))
     println(f0(1))
@@ -92,5 +128,27 @@ class KleisliTestOption extends FlatSpec with Matchers {
 
     val composed2 = Kleisli(f1).andThen(f0)
 
+  }
+
+  /**
+    * http://www.casualmiracles.com/2012/07/02/a-small-example-of-kleisli-arrows/
+    */
+  "composing two option-returning functions" should "work using for-comprehensions" in {
+
+    def foo(n: Int): Int => Option[Int] = { x => if (x == n) none else x.some }
+
+    val f0: (Int) => Option[Int] = foo(0)
+    val f1 = foo(1)
+
+    def oldSchool(i: Int) =
+      for (x <- f0(i);
+           y <- f1(x))
+        yield y
+
+    println(oldSchool(5))
+
+    val composed = Kleisli(f0).andThen(Kleisli(f1))
+
+    composed(5) shouldBe oldSchool(5)
   }
 }
