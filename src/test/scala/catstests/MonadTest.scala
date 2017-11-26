a0afa8d0f66bf066cb894cb4a2c08058e4eac83b
Ralf Oenning <ralf@invicto.net>
2017-11-26 15:58:42 +0000

    compose multiple async I/O operations using flatmpa

--- src/test/scala/catstests/MonadTest.scala
+++ src/test/scala/catstests/MonadTest.scala
@@ -2,6 +2,8 @@ package catstests
 
 import cc.Spec
 
+import scala.util.{Failure, Success, Try}
+
 /**
   * You often have several functions where in a sequence the next function needs the previous function's output as its input.
   * A => B, B => C
@@ -15,6 +17,16 @@ import cc.Spec
   * To do this you need something that takes the inside of F[B] and passes it to the next function.
   *
   *
+  * "Libraries that are fully non-blocking with asynchronous I/O operation usually encapsulated their return types in types like futures , one example of such libraries is reactivemongo. When using such libraries you will likely have to use thefuture ‘s flatmap method in composing multiple operations together."
+  *
+  *
+  * Looking carefully at the return types of the service methods(options), we can simply flatmap everything.
+val studentGrade = StudentService.getStudentById(id)
+                                 .flatMap(getCourses)
+                                 .flatMap(getGrades)
+
+
+
   *
   */
 class MonadTest extends Spec {
@@ -39,7 +51,7 @@ class MonadTest extends Spec {
   import StudentGrades._
 
 
-  "" in {
+  "a" in {
 
 
     val studentGrades = getStudentById(5)
