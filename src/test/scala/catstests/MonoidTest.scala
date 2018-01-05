242d4eac8a4dae31955689538ce9c0cdecffac40
Ralf Oenning <ralf@invicto.net>
2018-01-05 21:36:32 +0000

    revert

--- src/test/scala/catstests/MonoidTest.scala
+++ src/test/scala/catstests/MonoidTest.scala
@@ -37,15 +37,6 @@ object ValidationResult {
       case (_, i @ Invalid(_)) => i
     }
   }
-  implicit def monoidTuple[A: Monoid, B: Monoid]: Monoid[(A, B)] =
-    new Monoid[(A, B)] {
-      def combine(x: (A, B), y: (A, B)): (A, B) = {
-        val (xa, xb) = x
-        val (ya, yb) = y
-        (Monoid[A].combine(xa, ya), Monoid[B].combine(xb, yb))
-      }
-      def empty: (A, B) = (Monoid[A].empty, Monoid[B].empty)
-    }
 }
 
 
