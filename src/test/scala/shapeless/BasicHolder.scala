82bd36e3a098ccaa1ddcf07f1979bc959159aa2a
Ralf Oenning <ralf@invicto.net>
2018-01-07 11:46:51 +0000

    swap using var and match

--- src/test/scala/shapeless/BasicHolder.scala
+++ src/test/scala/shapeless/BasicHolder.scala
@@ -4,32 +4,46 @@ import cc.Spec
 
 class BasicHolder extends Spec {
 
-    case class One(fname: String, lname: String)
+  case class One(fname: String, lname: String)
 
   case class Holder(one: One) {
 
-      import record._
+    import record._
 
-      val gen = LabelledGeneric[One]
-      val er = gen.to(one)
+    val gen = LabelledGeneric[One]
+    var er = gen.to(one)
 
-      def apply(fn: Symbol) = fn match {
-        case 'fname => er('fname)
-        case 'lname => er('lname)
+    def apply(fn: Symbol) = fn match {
+      case 'fname => er('fname)
+      case 'lname => er('lname)
+    }
+
+    def update(fn: Symbol, ln: String) = {
+
+      fn match {
+        case 'fname => er = er.updated('fname, ln)
       }
 
+    }
   }
+
+  val one = One("1", "2")
+  val h = Holder(one)
 
   "create holder and get the value of a labelled field" in {
 
-    Holder(One("1", "2"))('fname) mustBe "1"
-    Holder(One("1", "2"))('lname) mustBe "2"
+    h('fname) mustBe "1"
+    h('lname) mustBe "2"
 
   }
 
+  "swap" in {
 
+    val ln = h('lname)
+    h update ('fname, ln)
+    println(h('fname))
 
-
+  }
 
 
 }
