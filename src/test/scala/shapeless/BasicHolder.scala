74d8ee2649f25c8f7ec9c91f59c18cbcb5ab8566
Ralf Oenning <ralf@invicto.net>
2018-01-07 01:02:52 +0000

    fix getters

--- src/test/scala/shapeless/BasicHolder.scala
+++ src/test/scala/shapeless/BasicHolder.scala
@@ -5,19 +5,25 @@ import cc.Spec
 class BasicHolder extends Spec {
 
     case class One(fname: String, lname: String)
-    case class Holder(one: One) {
+
+  case class Holder(one: One) {
 
       import record._
 
       val gen = LabelledGeneric[One]
       val er = gen.to(one)
 
-      def apply(fn: Symbol) = er('fname)
-    }
+      def apply(fn: Symbol) = fn match {
+        case 'fname => er('fname)
+        case 'lname => er('lname)
+      }
+
+  }
 
   "create holder and get the value of a labelled field" in {
 
-    Holder(One("1", "2"))('fname) mustBe 1
+    Holder(One("1", "2"))('fname) mustBe "1"
+    Holder(One("1", "2"))('lname) mustBe "2"
 
   }
 
