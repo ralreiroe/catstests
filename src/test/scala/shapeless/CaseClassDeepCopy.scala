8f3033fcf20ce9cad8346173212a3ec15e77d4bd
Ralf Oenning <ralf@invicto.net>
2018-01-12 15:56:40 +0000

    with and without implicit

--- src/test/scala/shapeless/CaseClassDeepCopy.scala
+++ src/test/scala/shapeless/CaseClassDeepCopy.scala
@@ -6,19 +6,29 @@ class CaseClassDeepCopy extends Spec {
 
   "https://stackoverflow.com/questions/33858745/automatically-convert-a-case-class-to-an-extensible-record-in-shapeless" in {
 
+    import shapeless._, shapeless.labelled.{ FieldType, field }
 
-//
-//    import shapeless._, shapeless.labelled.{ FieldType, field }
-//
-//    case class Address(street: String, zip: Int)
-//    case class Person(name: String, address: Address)
-//
-//    val person = Person("Jane", Address("street address", 12345))
-//
-//    type ShallowPersonRec =
-//      FieldType[Witness.`'name`.T, String] ::
-//        FieldType[Witness.`'address`.T, Address] :: HNil
-//
+    case class Address(street: String, zip: Int)
+
+    type ShallowPersonRec =
+      FieldType[Witness.`'name`.T, String] ::
+        FieldType[Witness.`'address`.T, Address] :: HNil
+
+    case class Person(name: String, address: Address)
+    val person = Person("Jane", Address("street address", 12345))
+
+    val shallowrec: ShallowPersonRec = LabelledGeneric[Person].to(person)
+
+    shallowrec mustBe "Jane" :: Address("street address",12345) :: HNil
+
+    def shallowRec[A](a: A)(implicit gen: LabelledGeneric[A]): gen.Repr = gen.to(a)
+
+    val shallowrec2 = shallowRec(person)
+
+    shallowrec2 mustBe "Jane" :: Address("street address",12345) :: HNil
+
+
+    //
 //    type DeepPersonRec =
 //      FieldType[Witness.`'name`.T, String] ::
 //        FieldType[
@@ -27,7 +37,6 @@ class CaseClassDeepCopy extends Spec {
 //            FieldType[Witness.`'zip`.T, Int] :: HNil
 //          ] :: HNil
 //
-//    val shallow: ShallowPersonRec = LabelledGeneric[Person].to(person)
 
   }
 
