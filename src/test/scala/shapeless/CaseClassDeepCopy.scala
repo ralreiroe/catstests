7d392335c1047baf429a4efcd285f1320de5169b
Ralf Oenning <ralf@invicto.net>
2018-01-12 16:17:17 +0000

    split

--- src/test/scala/shapeless/CaseClassDeepCopy.scala
+++ src/test/scala/shapeless/CaseClassDeepCopy.scala
@@ -1,32 +1,54 @@
 package shapeless
 
 import cc.Spec
+import shapeless.labelled.FieldType
 
+/**
+  * https://stackoverflow.com/questions/33858745/automatically-convert-a-case-class-to-an-extensible-record-in-shapeless
+  */
 class CaseClassDeepCopy extends Spec {
 
-  "https://stackoverflow.com/questions/33858745/automatically-convert-a-case-class-to-an-extensible-record-in-shapeless" in {
+  case class Address(street: String, zip: Int)
+  case class Person(name: String, address: Address)
 
-    import shapeless._, shapeless.labelled.{ FieldType, field }
+  val person = Person("Jane", Address("street address", 12345))
+  val address = Address("street address", 12345)
 
-    case class Address(street: String, zip: Int)
+  "shallow with explicit LabelledGeneric" in {
+
+    LabelledGeneric[Address].to(address) mustBe "street address" :: 12345 :: HNil
+    LabelledGeneric[Person].to(person) mustBe "Jane" :: Address("street address", 12345) :: HNil
+
+  }
+
+  "shallow with type alias" in {
 
     type ShallowPersonRec =
       FieldType[Witness.`'name`.T, String] ::
         FieldType[Witness.`'address`.T, Address] :: HNil
 
-    case class Person(name: String, address: Address)
-    val person = Person("Jane", Address("street address", 12345))
-
     val shallowrec: ShallowPersonRec = LabelledGeneric[Person].to(person)
 
-    shallowrec mustBe "Jane" :: Address("street address",12345) :: HNil
+    shallowrec mustBe "Jane" :: Address("street address", 12345) :: HNil
+
+  }
+
+  "shallow with implicit LabelledGeneric" in {
 
     def shallowRec[A](a: A)(implicit gen: LabelledGeneric[A]): gen.Repr = gen.to(a)
 
-    val shallowrec2: HList = shallowRec(person)
+    shallowRec(address) mustBe "street address" :: 12345 :: HNil
+    shallowRec(person) mustBe "Jane" :: Address("street address", 12345) :: HNil
 
-    shallowrec2 mustBe "Jane" :: Address("street address",12345) :: HNil
+  }
 
+
+
+  "deep" in {
+
+    //TODO
+
+//  * https://stackoverflow.com/questions/33858745/automatically-convert-a-case-class-to-an-extensible-record-in-shapeless
 
     //
 //    type DeepPersonRec =
