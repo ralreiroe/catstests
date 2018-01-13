873e2525df7451503d55a672073ea8b5ebc95c58
Ralf Oenning <ralf@invicto.net>
2018-01-13 15:33:10 +0000

    working

--- src/main/scala/autoconversion/MapCaseClass.scala
+++ src/main/scala/autoconversion/MapCaseClass.scala
@@ -1,7 +1,7 @@
 package autoconversion
 
 
-import shapeless.{HList, ::, HNil, LabelledGeneric}
+import shapeless.{::, HList, HNil, LabelledGeneric, Witness}
 import shapeless.labelled.{FieldType, field}
 import shapeless.ops.record.Selector
 
@@ -21,15 +21,18 @@ object FieldsSelector {
 
   implicit def hListFieldsSelector[Value, Key, TargetHListTail <: HList, SourceHList <: HList](
     implicit
-    restFieldsSelect: FieldsSelector[SourceHList, TargetHListTail],
-    select: Selector.Aux[SourceHList, Key, Value] // select the value of type Value labelled with Key from a HList of type Source
-  ): FieldsSelector[SourceHList, FieldType[Key, Value] :: TargetHListTail] =
+    restFieldsSelect: FieldsSelector[SourceHList, TargetHListTail]
+    ,
+    select: Selector.Aux[SourceHList, Key, Value]                     // select the value of type Value labelled with Key from a HList of type Source
+   )
+
+   : FieldsSelector[SourceHList, FieldType[Key, Value] :: TargetHListTail] =
 
     //(2) FieldSelector on HList, FieldType[Key, Value] :: HList
 
     new FieldsSelector[SourceHList, FieldType[Key, Value] :: TargetHListTail] {
-      def apply(source: SourceHList): FieldType[Key, Value] :: TargetHListTail =
-        field[Key](select(source)) :: restFieldsSelect(source)
+
+      def apply(source: SourceHList): FieldType[Key, Value] :: TargetHListTail = field[Key](select(source)) :: restFieldsSelect(source)
     }
 }
 
@@ -50,9 +53,6 @@ object CaseClassMap {
   }
 }
 
-
-
-
 object MapCaseClass extends App {
 
   implicit class MappedToSyntax[Source](val source: Source) {
@@ -67,14 +67,6 @@ object MapCaseClass extends App {
   //  import MappedToSyntax._
   val res: Class2 = Class1(1, "quux").mappedTo[Class2] // Class2("quux", 1)
   println(res)
-
-
-
-
-
-
-
-
 
 
   case class One2(name: String, less18: Boolean, more18: Boolean, fname: String)
