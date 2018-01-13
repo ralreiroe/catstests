d75ca501a0b55aa3ddde8912172d9b22df7f2118
Ralf Oenning <ralf@invicto.net>
2018-01-13 19:41:49 +0000

    renames

--- src/main/scala/autoconversion/MapCaseClass.scala
+++ src/main/scala/autoconversion/MapCaseClass.scala
@@ -1,39 +1,40 @@
 package autoconversion
 
 
-import shapeless.{::, HList, HNil, LabelledGeneric, Witness}
-import shapeless.labelled.{FieldType, field}
+import shapeless.{::, HList, HNil, LabelledGeneric}
+import shapeless.labelled.{FieldType}
 import shapeless.ops.record.Selector
 
 
-trait FieldsSelector[SourceHList, TargetHList] {
+trait ListBuilder[SourceHList, TargetHList] {
   def apply(source: SourceHList): TargetHList
 }
 
-object FieldsSelector {
-  implicit def hNilFieldsSelector[SourceHList]: FieldsSelector[SourceHList, HNil] =
+object ListBuilder {
+  implicit def emptyTgtListHeadExtractor[SourceHList] =
 
-    //(1) FieldSelector on HList, HNil
-
-    new FieldsSelector[SourceHList, HNil] {
-      def apply(dontCare: SourceHList): HNil = HNil
+    new ListBuilder[SourceHList, HNil] {
+      def apply(notNeeded: SourceHList): HNil = HNil
     }
 
-  implicit def hListFieldsSelector[Value, Key, TargetHListTail <: HList, SourceHList <: HList](
-    implicit
-    restFieldsSelect: FieldsSelector[SourceHList, TargetHListTail]
-    ,
-    select: Selector.Aux[SourceHList, Key, Value]                     // select the value of type Value labelled with Key from a HList of type Source
-   )
+  /**
+    */
 
-   : FieldsSelector[SourceHList, FieldType[Key, Value] :: TargetHListTail] =
+  implicit def nonEmptyTgtListHeadExtractor[TFn, TValueType, FromList <: HList, ToList <: HList](
+                                                                                                  implicit
+                                                                                                  listBuilder: ListBuilder[FromList, ToList]
+                                                                                                  ,
+                                                                                                  select: Selector.Aux[FromList, TFn, TValueType]
+                                                                                                )
 
-    //(2) FieldSelector on HList, FieldType[Key, Value] :: HList
+  = new ListBuilder[FromList, FieldType[TFn, TValueType] :: ToList] {
 
-    new FieldsSelector[SourceHList, FieldType[Key, Value] :: TargetHListTail] {
+    def apply(source: FromList) = {
 
-      def apply(source: SourceHList): FieldType[Key, Value] :: TargetHListTail = field[Key](select(source)) :: restFieldsSelect(source)
+      val srcFnAndType: FieldType[TFn, TValueType] = select(source).asInstanceOf[FieldType[TFn, TValueType]]
+      srcFnAndType :: listBuilder(source)
     }
+  }
 }
 
 trait CaseClassMap[Source, Target] {
@@ -41,41 +42,30 @@ trait CaseClassMap[Source, Target] {
 }
 
 object CaseClassMap {
-  implicit def caseClassMap[Source, Target, SourceRepr <: HList, TargetRepr <: HList](
-     implicit
-     sourceGen: LabelledGeneric.Aux[Source, SourceRepr],
-     targetGen: LabelledGeneric.Aux[Target, TargetRepr],
-     labelledHListMapper: FieldsSelector[SourceRepr, TargetRepr]
-   ): CaseClassMap[Source, Target] =
+  implicit def caseClassMap[FromType, ToType, FromList, ToList](
+                                                                 implicit
+                                                                 fromlgen: LabelledGeneric.Aux[FromType, FromList],
+                                                                 tolgen: LabelledGeneric.Aux[ToType, ToList],
+                                                                 listBuilder: ListBuilder[FromList, ToList]
+                                                               ) =
 
-    new CaseClassMap[Source, Target] {
-      def apply(source: Source): Target = targetGen.from(labelledHListMapper(sourceGen.to(source)))
-  }
+    new CaseClassMap[FromType, ToType] {
+      def apply(inst: FromType) = tolgen.from(listBuilder(fromlgen.to(inst)))
+    }
 }
 
 object MapCaseClass extends App {
 
-  implicit class MappedToSyntax[Source](val source: Source) {
-    def mappedTo[Target](implicit ccMap: CaseClassMap[Source, Target]): Target = ccMap(source)
+  implicit class MappedToSyntax[From](val inst: From) {
+    def mappedTo[To](implicit ccMap: CaseClassMap[From, To]): To = ccMap(inst)
   }
 
+  case class SourceCC(fname: String, less18: Boolean, more18: Boolean, name: String)
 
-  case class Class1(intValue: Int, stringValue: String)
-  case class Class2(stringValue: String, intValue: Int)
+  case class TargetCC(less18: Boolean, name: String, more18: Boolean)
 
-
-  //  import MappedToSyntax._
-  val res: Class2 = Class1(1, "quux").mappedTo[Class2] // Class2("quux", 1)
-  println(res)
-
-
-  case class One2(fname: String, less18: Boolean, more18: Boolean, name: String)
-  case class Two2(less18: Boolean, name: String, more18: Boolean)
-
-  val res2: Two2 = One2("", false, true, "quux").mappedTo[Two2]
+  val res2: TargetCC = SourceCC("", false, true, "quux").mappedTo[TargetCC]
   println(res2)
-
-
 
 
 }
