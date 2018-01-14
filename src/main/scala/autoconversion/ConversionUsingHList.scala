8fbf6e37c1a2fd3ce0f2d8ca5b0508e00e21eee2
Ralf Oenning <ralf@invicto.net>
2018-01-14 13:37:20 +0000

    almost final

--- src/main/scala/autoconversion/ConversionUsingHList.scala
+++ src/main/scala/autoconversion/ConversionUsingHList.scala
@@ -3,48 +3,63 @@ package autoconversion
 import shapeless.labelled.FieldType
 import shapeless.{::, HList, HNil}
 
-trait HListCopier[Elements] {
-
-  def apply(elements: Elements): Elements
+trait Copier[T] {
+  def apply(elements: T): T
 }
 
-object HListCopier {
+object Copier {
 
-  implicit def hNilCopier: HListCopier[HNil] = new HListCopier[HNil] {
+  implicit def empty: Copier[HNil] = new Copier[HNil] {
     def apply(elements: HNil): HNil = HNil
   }
 
-  implicit def nonEmptyHListCopier[A, Tail <: HList](implicit hlistCopier: HListCopier[Tail]) =
-    new HListCopier[A :: Tail] {
-      def apply(elements: A :: Tail): A :: Tail = elements.head :: hlistCopier(elements.tail)
+  implicit def nonEmpty[T1, T2 <: HList](implicit copier: Copier[T2]) =
+    new Copier[T1 :: T2] {                                                                  //pattern match <====================
+      def apply(elements: T1 :: T2): T1 :: T2 = elements.head :: copier(elements.tail)
     }
 }
 
 object HListCopy {
 
-  def copyHList[Source <: HList](source: Source)(implicit hListCopier: HListCopier[Source]): Source =
-    hListCopier(source)
+  def copyHList[T](source: T)(implicit copier: Copier[T]): T = copier.apply(source)
 }
 
 
 object ConvertUsingHList extends App {
 
-  val hlist = 1 :: "quux" :: HNil
-  val copy = HListCopy.copyHList(hlist)
+  val hlist: Int::String::HNil = 1 :: "quux" :: HNil
+  val copy = HListCopy.copyHList(hlist)   //(1) copyHList requires a Copier[Int::String::HNil] which only nonEmpty provides (line 17)
   println(copy)
-
-
-  //illustration how the compiler insert calls to the implicit defs HListCopier.nonEmptyHListCopier and HListCopier.hNilCopier f
-  // or the implicit arg required by copyHList
-  val copy2 = HListCopy.copyHList(hlist)(
-      HListCopier.nonEmptyHListCopier(
-        HListCopier.nonEmptyHListCopier(
-          HListCopier.hNilCopier
+  //Again in line 17, the type parameters are replaced by the real types: Int replaces T1, String::HNil replaces T2
+  val copyWithOneImplicitInserted = HListCopy.copyHList[Int :: String :: HNil](hlist)(
+    Copier.nonEmpty[Int, String :: HNil]
+  )
+  val copyWithTwoImplicitsInserted = HListCopy.copyHList[Int :: String :: HNil](hlist)(
+    Copier.nonEmpty[Int, String :: HNil](
+      Copier.nonEmpty[String, HNil]
+    )
+  )
+  val copyWithThreeImplicitsInserted = HListCopy.copyHList[Int :: String :: HNil](hlist)(
+    Copier.nonEmpty[Int, String :: HNil](
+      Copier.nonEmpty[String, HNil](
+        Copier.empty
       )
     )
   )
 
-  println(copy2)
+
+  //illustration how the compiler insert calls to the implicit defs HListCopier.nonEmptyHListCopier and HListCopier.hNilCopier f
+  // or the implicit arg required by copyHList
+  val copy3 = HListCopy.copyHList(hlist)(
+      Copier.nonEmpty(
+        Copier.nonEmpty(
+          Copier.empty
+      )
+    )
+  )
+
+  println("copy: "+copy)
+  println("copyWithThreeImplicitsInserted: " +copyWithThreeImplicitsInserted)
 
 
   import shapeless._ ; import syntax.singleton._
