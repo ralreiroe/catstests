952b20309ce456967d511d3e0bdf070904267305
Ralf Oenning <ralf@invicto.net>
2018-02-11 11:16:30 +0000

    further illus

--- src/test/scala/stringRows/TestStringConversion.scala
+++ src/test/scala/stringRows/TestStringConversion.scala
@@ -8,9 +8,10 @@ class TestStringConversion extends Spec {
   type S = String :: String :: HNil
   type H = Double :: Int    :: HNil
 
+  val stringsHL: S = "1234.0" :: "1234" :: HNil
+
   "a" in {
 
-    val stringsHL: S = "1234.0" :: "1234" :: HNil
 
     When("we summon an HLConversion[S, H]")
     var x: Option[H] = null
@@ -27,8 +28,18 @@ class TestStringConversion extends Spec {
       )
     ).apply(stringsHL)
     x mustBe Some(1234.0 :: 1234 :: HNil)
+  }
 
+  "b" in {
 
+    val l = stringsHL
+    val l0: String = stringsHL.head
+    val l1: String = stringsHL.tail.head
+
+    for {
+      d <- StringConversion.convertToInt(l0)
+      i <- StringConversion.convertToString(l1)
+    } yield d :: i :: HNil
 
 
   }
